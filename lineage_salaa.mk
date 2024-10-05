ed6405528e81ce524f3fb94e60c212154e0c193b
Stim Luks <san10031987san@gmail.com>
2024-10-06 00:35:44 +0500

    first commit

---
+++ lineage_salaa.mk
@@ -0,0 +1,72 @@
+#
+#  Copyright (C) 2024 The LineageOS Project
+#
+#  SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Inherit some common EVO stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Certification
+$(call inherit-product-if-exists, vendor/certification/config.mk)
+
+# Extras
+$(call inherit-product, vendor/extras/evolution.mk)
+
+# Bcr
+$(call inherit-product, vendor/extras/bcr/bcr.mk)
+
+# EVO flags
+EVO_BUILD_TYPE := Unofficial
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+
+# GMS
+WITH_GMS := true
+
+# Charging Animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# EPPE
+#TARGET_DISABLE_EPPE := true
+
+# Device Information
+PRODUCT_DEVICE := salaa
+PRODUCT_NAME := lineage_$(PRODUCT_DEVICE)
+PRODUCT_BRAND := realme
+PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
+PRODUCT_MODEL := realme 7/Narzo 20 Pro/Narzo 30 4G
+PRODUCT_SYSTEM_NAME := RMX2155
+PRODUCT_SYSTEM_DEVICE := RMX2155L1
+
+PRODUCT_GMS_CLIENTID_BASE := android-$(PRODUCT_BRAND)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    PRODUCT_NAME=$(PRODUCT_SYSTEM_NAME) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+# Fingerprint
+BUILD_FINGERPRINT := "realme/RMX2155/RMX2155L1:12/SP1A.210812.016/Q.GDPR.bf75e7-1:user/release-keys"
+BUILD_DESCRIPTION := "sys_mssi_64_cn_armv82-user 12 SP1A.210812.016 1668670161293 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC=$(BUILD_DESCRIPTION)
---
+++ vendorsetup.sh
@@ -0,0 +1,34 @@
+# Cloning required repositories for making rum:
+
+# Kernel Source:
+git clone  https://github.com/StimLuks87/kernel_oplus_mt6785 kernel/realme/mt6785
+
+# Vendor Source:
+git clone https://github.com/StimLuks87/vendor_device_oplus_ossi -b VIC vendor/realme/salaa
+unzip vendor/realme/salaa/radio/md1img.zip -d vendor/realme/salaa/radio && rm vendor/realme/salaa/radio/md1img.zip
+unzip vendor/realme/salaa/proprietary/odm/lib64/libstfaceunlockppl.zip -d vendor/realme/salaa/proprietary/odm/lib64 && rm vendor/realme/salaa/proprietary/odm/lib64/libstfaceunlockppl.zip
+
+# Hardware of Mediatek and Oplus:
+git clone https://github.com/Evolution-X-Devices/hardware_oplus hardware/oplus
+git clone https://github.com/Project-PixelStar/hardware_mediatek hardware/mediatek
+
+# Mediatek SEPolicy VNDR:
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+
+# Lineage SEPolicy:
+git clone https://github.com/LineageOS/android_device_lineage_sepolicy device/lineage/sepolicy
+
+# ViPER4AndroidFX:
+git clone https://github.com/StimLuks87/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+
+# CameraGo
+unzip device/realme/salaa/CameraGo/CameraGo.zip -d device/realme/salaa/CameraGo && rm device/realme/salaa/CameraGo/CameraGo.zip
+
+# Make the build faster using ccache
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 50G
+ccache -o compression=true
+
+# Disable and stop systemd-oomd service.
+systemctl disable --now systemd-oomd
